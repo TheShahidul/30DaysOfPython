0cbd07ef924b69ee6dc7a0dabae6a006d65279f0
theShahidul <theshahidul.edu@gmail.com>
2024-06-01 15:01:18 +0600

    day 3

---
+++ Day3.py
@@ -0,0 +1,27 @@
+#if statements
+
+is_hot = True
+if is_hot:
+    print("it's a hot day")
+    print("drink plenty of water")
+print("enjoy your day")
+print('----------------------')
+
+is_hot = False
+if is_hot:
+    print("it's a hot day")
+print("enjoy your day")
+print('----------------------')
+
+is_hot = False #change the value to false and see the difference
+is_cold = True # change to false and see the difference
+if is_hot:
+    print("it's a hot day")
+    print("drink plenty of waters")
+elif is_cold:
+    print("it's a cold day")
+    print("wear warm clothes")
+else:
+    print("it's a lovely day")
+
+print("enjoy your day")
